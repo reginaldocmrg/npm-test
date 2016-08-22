b464e47033ca63b7dfd23fdcc7649c939bf4e13c
reginaldocmrg <reginaldocmrg@hotmail.com>
2016-08-22 11:31:43 -0300

    Added missing files

--- dist/reginaldocmrg-npm-test.js
+++ dist/reginaldocmrg-npm-test.js
@@ -3,4 +3,4 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require('./core'));
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=reginaldocmrg-npm-test.js.map
