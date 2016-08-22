c23d717b6bcb0fa7193e3f0efa374e47cf79d810
reginaldocmrg <reginaldocmrg@hotmail.com>
2016-08-22 09:42:29 -0300

    Created Math2 test class

---
+++ core/src/Math2.ts
@@ -0,0 +1,13 @@
+export class Math2 {
+    public static sum(num1: number, num2: number): number {
+        return num1 + num2;
+    }
+
+    public static minus(num1: number, num2: number): number {
+        return num1 - num2;
+    }
+
+    public static multiply(num1: number, num2: number): number {
+        return num1 * num2;
+    }
+}
